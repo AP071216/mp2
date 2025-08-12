a617fbdcf4ecbc6dde63cc5f6e4fbc5ab29937b1
ap071216 <alicephipps12@gmail.com>
2025-08-12 15:27:12 +0100

    Add function to shuffle the cards when the game is started

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,14 @@ document.addEventListener('DOMContentLoaded', () => {
     let tries = 0;
 });
 
+/**
+ * Shuffles the cards when the game is started
+ */
+function shuffleCards(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
